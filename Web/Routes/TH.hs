0cae2d6057eb7ac2a1b8dcfe2c78339c600336c5
Jeremy Shaw <jeremy@n-heptane.com>
2022-04-20 10:38:45 -0500

    fix build for template-haskell >= 2.17. drop support for template-haskell < 2.11

--- Web/Routes/TH.hs
+++ Web/Routes/TH.hs
@@ -87,23 +87,18 @@ parseInfo :: Name -> Q Class
 parseInfo name
     = do info <- reify name
          case info of
-#if MIN_VERSION_template_haskell(2,11,0)
            TyConI (DataD cx _ keys _ cs _)    -> return $ Tagged (map conInfo cs) cx $ map conv keys
            TyConI (NewtypeD cx _ keys _ con _)-> return $ Tagged [conInfo con] cx $ map conv keys
-#else
-           TyConI (DataD cx _ keys cs _)    -> return $ Tagged (map conInfo cs) cx $ map conv keys
-           TyConI (NewtypeD cx _ keys con _)-> return $ Tagged [conInfo con] cx $ map conv keys
-#endif
-           _                                ->  error $ "derivePathInfo - invalid input: " ++ pprint info
     where conInfo (NormalC n args) = (n, length args)
           conInfo (RecC n args) = (n, length args)
           conInfo (InfixC _ n _) = (n, 2)
           conInfo (ForallC _ _ con) = conInfo con
-#if MIN_VERSION_template_haskell(2,4,0)
+#if MIN_VERSION_template_haskell(2,17,0)
+          conv (PlainTV nm _) = nm
+          conv (KindedTV nm _ _) = nm
+#else
           conv (PlainTV nm) = nm
           conv (KindedTV nm _) = nm
-#else
-          conv = id
 #endif
 
 -- | the standard formatter
@@ -138,11 +133,7 @@ parseMethods :: Name -> Q [Name]
 parseMethods con =
     do info <- reify con
        case info of
-#if MIN_VERSION_template_haskell(2,11,0)
          (DataConI _ ty _) ->
-#else
-         (DataConI _ ty _ _) ->
-#endif
              do runIO $ print ty
                 runIO $ print $ lastTerm ty
                 return $ extractMethods (lastTerm ty)
